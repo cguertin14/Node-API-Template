fa95d47a75e6fcfcddd5ef99438890863bf323c2
Charles Guertin <charlesguertin@live.ca>
2018-05-02 18:14:20 -0400

    Rate Limiter implemented

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ import './config/passport.js';
 import './db/mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
+import rateLimiter from './config/rateLimiter';
 import userRoutes from './api/routes/users';
 
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(rateLimiter);
 
 // Routes.
 app.use('/users', userRoutes);
